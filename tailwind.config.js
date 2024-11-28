1b5aea90e7609e42bff0fedcfe81454963d71cdb
Charlotte Balang <166142143+LottieMe@users.noreply.github.com>
2024-11-28 09:35:57 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',       
+    './css/**/*.css',     
+    './node_modules/daisyui/dist/**/*.js',  
+  ],
+  plugins: [
+    require('daisyui'),   
+  ],
+}
